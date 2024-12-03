175e6fc4162f558103d0132a780e7920cd66b77e
Sarunyamk <Sarunya46mk@gmail.com>
2024-12-03 17:06:09 +0700

    test git

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ const page = () => {
   return (
     <div>
       <h1>Page</h1>
-      <Button variant="destructive" size={"default" }>Button</Button>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, minus itaque laborum nihil impedit, non similique sapiente enim consequuntur eum repellendus minima deleniti obcaecati praesentium. Repellendus reprehenderit sint debitis sit.</h1>
+
+      <Button variant="destructive" size={"default"}>Button</Button>
 
     </div>
   )
